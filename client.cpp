54c02cadb2843b550e4ee08abb666b0b0beaf045
Renato Vargas <renatolvargas@gmail.com>
2017-04-20 12:12:20 -0300

    First version of client

--- client.cpp
+++ client.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  authors: Tiago Lobato Gimenes (118827)
-//           Ingrato
+//           Renato Landim Vargas (118557)
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -15,13 +15,16 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
 #include <unistd.h>
 #include <netdb.h>
-#include <assert.h>
-#include <stdbool.h>
+#include <cassert>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "utils.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -36,28 +39,69 @@ int main(int argc, char * argv[])
   struct sockaddr_in socket_address;
   char *host;
   char buf[MAX_LINE];
-  int s;
+  std::string str_in;
+  int server_fd;
   int len;
 
-  /* verificação de argumentos */
-  assert(false);
+  // Checks given arguments
+  if (argc < 2) {
+      log::write(FAIL, "Missing hostname argument");
+  }
+  else if (argc > 2) {
+      log::write(FAIL, "Too many arguments");
+  }
 
-  /* tradução de nome para endereço IP */
-  assert(false);
+  // Gets address from host name
+  host = argv[1]; 
+  host_address = gethostbyname(host);
 
-  /* criação da estrutura de dados de endereço */
+  if (host_address == NULL) {
+      log::write(FAIL, "No host found with given name");
+  }
+
+  // Creates socket address structure
   bzero((char *)&socket_address, sizeof(socket_address));
-  assert(false);
+  socket_address.sin_family = AF_INET;
+  socket_address.sin_port = htons(SERVER_PORT);
+  //socket_address.sin_addr = *(struct in_addr *)host_address->h_addr;
+  bcopy((char *)host_address->h_addr, (char *)&socket_address.sin_addr.s_addr, host_address->h_length);
 
-  /* criação de socket ativo*/
-  assert(false);
+  // Creates active socket
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  /* estabelecimento da conexão */
-  assert(false);
+  // Connects to server
+  int ret = connect(server_fd, (struct sockaddr*)&socket_address, sizeof(socket_address));
 
-  /* ler e enviar linhas de texto, receber eco */
-  assert(false);
+  if (ret >= 0) {
+      log::write(DEBUG, "Socket " + std::to_string(server_fd) + " was connected");
+  }
+  else {
+      log::write(FAIL, std::strerror(errno));
+  }
 
+  do {
+    bzero(buf, sizeof(char)*MAX_LINE);
+    std::getline(std::cin, str_in);
+    len = str_in.size();
+    
+    int n = write(server_fd, str_in.c_str(), len);
+    
+    if (n <= 0) {
+        log::write(FAIL, "Fail to write to server");
+    }
+
+    n = read(server_fd, buf, MAX_LINE);
+    
+    if (n <= 0) {
+        log::write(FAIL, "Fail to read from server");
+    }
+
+    std::cout << std::string(buf) << std::endl;
+  } while(std::strcmp(buf, "exit\r\n") && std::strcmp(buf, "exit\n"));
+
+  close(server_fd);
+
+  return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
